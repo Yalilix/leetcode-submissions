aa3a9fdb3828c3a3a33f32ee5c26d285acc7d6fe
Yanlin Li <148876808+Yalilix@users.noreply.github.com>
2026-06-17 00:13:36 +1000

    Time: 0 ms (100.00%) | Memory: 3.5 MB (100.00%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.rs
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.rs
@@ -2,26 +2,17 @@ use std::iter;
 
 impl Solution {
     pub fn process_str(s: String) -> String {
-        let mut ret = String::new();
+        let mut result = String::new();
         
         for c in s.chars() {
-            if c == '*' { let _ = ret.pop(); }
-            else if c == '#' {
-                let copy = ret.clone();
-                ret.push_str(&copy);
+            match c {
+                '*' => { result.pop(); }
+                '#' => { result.extend_from_within(0..); }
+                '%' => { result = result.chars().rev().collect(); }
+                _ => { result.push(c); }
             }
-            else if c == '%' {
-                ret = ret
-                    .chars()
-                    .rev()
-                    .collect();
-            }
-            else { 
-                println!("{c}");
-                ret.push(c);
-             }
         }
 
-        ret
+        result
     }
 }
