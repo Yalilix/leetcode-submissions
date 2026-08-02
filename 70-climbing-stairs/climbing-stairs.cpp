6fdadbe8b29f7ba630902f53cfa8c2cf3ac7ce74
Yanlin Li <148876808+Yalilix@users.noreply.github.com>
2026-08-03 02:14:38 +1000

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.65%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     int climbStairs(int n) {
         if (n <= 2) return n;
-        vector<long long int> dp(n + 1, 1);
-        for (int i = 2; i <= n; i++) {
+        vector<long long int> dp(n + 1);
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
-        }  
+        } 
 
         return dp[n];
     }
