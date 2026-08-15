1c6451e89dc8789d4ea5914b8eb5271ee3fb3529
Yanlin Li <148876808+Yalilix@users.noreply.github.com>
2026-08-16 03:14:24 +1000

    Time: 90 ms (29.16%) | Memory: 26.1 MB (23.16%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+        if (s.size() <= 1) return s;
+
         int n = s.size();
+
         int maxLen = 0;
-        string ret = "";
-
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+        int start = 0, end = 0;
+        
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                string cur = "";
-                int l = i, r = j;
-
-                while (l <= r) {
-                    if (s[l] != s[r]) break;
-                    l++; r--;
-                }
-
-                if (l > r) {
-                    cur = s.substr(i, j - i + 1);
-                }
-
-                if (cur.size() > maxLen) {
-                    ret = cur;
-                    maxLen = cur.size();
+            dp[i][i] = true;
+            for (int j = 0; j < i; j++) {
+                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > maxLen) {
+                        maxLen = i - j + 1;
+                        start = j; end = i;
+                    }
                 }
             }
         }
 
-        return ret;
+        return s.substr(start, end - start + 1);
     }
 };
